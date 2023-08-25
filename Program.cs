af83f3ba02a8c9cfff686593a6734e461fb5346c
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:48:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1POOex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Milhas l;
+            l = new Milhas();
+
+            Console.WriteLine("valor de milhas");
+            l.setM(double.Parse(Console.ReadLine()));
+
+            l.calcular();
+
+            Console.WriteLine("valor convertido = {0}", l.getA());
+        
+        }
+    }
+}
